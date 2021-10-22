b48cb66f17ae4cb63f13b47d5116cb5072b51665
danielpetterson <dpet922@hotmail.com>
2021-10-23 09:24:57 +1030

    update

--- test.py
+++ test.py
@@ -1,81 +1,39 @@
+import os
 import torch
 from torchvision import transforms, datasets
-# from torchvision import transforms, datasets
-# from torch.utils import data
-# import torch.nn as nn
-# import matplotlib.pyplot as plt
+from model import CNN
+import torch.utils as ut
 
-# Define model
-class CNN(nn.Module):
-    def __init__(self):
-        super(CNN, self).__init__()
-        self.conv1 = nn.Sequential(
-            nn.Conv2d(3, 256, kernel_size=12, stride=4, padding=1),
-            nn.MaxPool2d(kernel_size=3, stride=2),
-            nn.ReLU())
-        self.conv2 = nn.Sequential(
-            nn.Conv2d(256, 128, kernel_size=6, stride=1, padding=2),
-            nn.BatchNorm2d(128),
-            nn.ReLU())
-        self.conv3 = nn.Sequential(
-            nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(128),
-            nn.ReLU())
-        self.fc = nn.Sequential(
-            nn.Flatten(),
-            nn.Linear(86528,512),
-            nn.ReLU(),
-            nn.Dropout(p=0.2),
-            nn.Linear(512,256),
-            nn.ReLU(),
-            nn.Dropout(p=0.2),
-            nn.Linear(256,64),
-            nn.ReLU(),
-            nn.Linear(64, 3))
 
-    def forward(self, input):
-        out = self.conv1(input)
-        out = self.conv2(out)
-        out = self.conv3(out)
-        out = self.fc(out)
-        
-        return out
+if __name__ == "__main__":
+    # Instantiate model and load weights
+    model = CNN()
+    location = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+    model.load_state_dict(torch.load(os.path.join(location, "model.pth")))
 
-def import_preprocess_test(path):\
-
-    # Define data transformations
+        # Define data transformations
     data_transform = transforms.Compose([
+        # Scale to same size as training images
         transforms.Resize([230, 230]),
         transforms.ToTensor(),
+        # Normalise to same values as training data
         transforms.Normalize([0.5],[0.5])
     ])
 
     # Import dataset and apply transformation
-    dataset = datasets.ImageFolder(root=path, transform=data_transform)
-
-    return dataset
-
-if __name__ == "__main__":
-    path = "/Users/danielpetterson/GitHub/PytorchImageClassification/Data/traindata"
-    # Hyperparameters
-    num_epochs = 3
-    num_classes = 3
-    batch_size = 10
-    learning_rate = 0.001
-
-    # Instantiate model
-    model = CNN()
-
-    # Loss and optimizer
-    criterion = torch.nn.CrossEntropyLoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
-    test_data = import_preprocess_test(path, batch_size)
+    path = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__), "testdata"))
+    test_data = datasets.ImageFolder(root=path, transform=data_transform)
+    test_loader = ut.data.DataLoader(test_data, batch_size=32)
+    dataiter = iter(test_loader)
+    data, labels = dataiter.next()
     
-    model.load_state_dict(torch.load("savedModel.pth"))
-    for param_tensor in model.state_dict():
-        print(param_tensor, "\t", model.state_dict()[param_tensor].size())
+    # Calculate accuracy on test set
+    correct = 0
+    total = 0
     model.eval()
-    for data, labels in test_data:
-            target = model(data)
-            loss = criterion(target,labels)
-            valid_loss = loss.item()
+    outputs = model(data)
+    _, predicted = torch.max(outputs.data, 1)
+    total += outputs.size(0)
+    correct += (predicted == labels).sum().item()
+    accuracy = (correct / total) * 100.0
+    print('Accuracy on test set: %d %%' % (accuracy))
